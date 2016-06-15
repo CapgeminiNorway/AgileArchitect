72db187c78202c477d08751a1477bea02412062c
anpreg <annamaria.preg@capgemini.com>
2016-06-15 22:41:10 +0200

    Testing mkdocs...

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 # Install go if needed
 export TOOLS_ROOT_DIR=$HOME/site/deployments/tools
-export MKDOCS_DIR=$TOOLS_ROOT_DIR/Python35/Scripts/
+export MKDOCS_DIR=$TOOLS_ROOT_DIR/Python35/Scripts
 export PATH=$MKDOCS_DIR
 
 echo $PATH
+ls -l $TOOLS_ROOT_DIR
+ld -l $MKDOCS_DIR
 mkdocs.exe --version
 $MKDOCS_DIR/mkdocs.exe --version
 #export BASE_URL="http://$WEBSITE_HOSTNAME"
