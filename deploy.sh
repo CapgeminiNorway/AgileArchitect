4351d487a4f64fe65066b19c517591e02f8f05bc
anpreg <annamaria.preg@capgemini.com>
2016-06-16 13:36:09 +0200

    mkdocs test

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 # Install go if needed
 #export TOOLS_ROOT_DIR=$HOME/site/deployments/tools
-#export MKDOCS_DIR=$TOOLS_ROOT_DIR/Python35/Scripts
+export MKDOCS_DIR=$HOME/Python35/Scripts
 #export PYTHON_DIR=$TOOLS_ROOT_DIR/Python35
 #export PATH=$PYTHON_DIR:$MKDOCS_DIR:$PATH
 
-export PIP_ROOT=D:/Python34/Scripts
-export PATH=$PIP_ROOT:$PATH
+export PATH=$MKDOCS_DIR:$PATH
 
 echo $PATH
-ls -l $PIP_ROOT
-cd $PIP_ROOT
-$PIP_ROOT/pip install mkdocs
-ls -l $PIP_ROOT
+ls -l $MKDOCS_DIR
+cd $MKDOCS_DIR
+
 mkdocs --version
-$PIP_ROOT/mkdocs --version
-$PIP_ROOT/mkdocs.exe --version
+$MKDOCS_DIR/mkdocs --version
+$MKDOCS_DIR/mkdocs.exe --version
 #export BASE_URL="http://$WEBSITE_HOSTNAME"
 #if [ "$WEBSITE_HOSTNAME" == "opencontainers.azurewebsites.net" ]; then
 #  export BASE_URL="https://www.opencontainers.org"
